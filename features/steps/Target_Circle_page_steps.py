8a6457ef25c78f75c70216335a4bfb209ee5cadd
Michael Trisotto <mjt08d@gmail.com>
2024-04-08 09:47:18 -0500

    Updated 6_links_Target_circle_page

--- features/steps/Target_Circle_page_steps.py
+++ features/steps/Target_Circle_page_steps.py
@@ -3,15 +3,15 @@ from behave import given, when, then
 from time import sleep
 
 
-HEADER_LINKS = (By.CSS_SELECTOR, "a[id*='utilityNav']")
+LINKS = (By.CSS_SELECTOR, "[data-lnk='Join Circle '],[data-lnk='Circle Deals '],[data-lnk='Bonus'],[data-lnk='Partner'],[data-lnk='Voting '],[data-lnk='Circe FAQs']")
 
 @given('Open Target Circle page')
 def open_target_circle_page(context):
     context.driver.get('https://www.target.com/l/target-circle/-/N-pzno9')
 
 
-@then('Verify 6 links are present on "Navigation bar"')
+@then('Verify 6 links are present on "Circle Page"')
 def verify_navigation_bar(context):
-    links = context.driver.find_elements(*HEADER_LINKS)
+    links = context.driver.find_elements(*LINKS)
     print(links)
     assert len(links) == 6, f'Expected 6 links but got {len(links)}'
